a403937158ddabb77a9ffac483fa6a24e799b177
Dick Hon <honchunkin@gmail.com>
2023-09-06 05:21:20 +0800

    feat: add test case for controller

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -4,17 +4,44 @@ import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
+
+  const mockUserData = {
+    name: 'John',
+    email: 'john@example.com',
+    dob: new Date(),
+    phoneNumber: '123456',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([mockUserData]),
+            create: jest.fn().mockResolvedValue(mockUserData),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a list of users', async () => {
+    expect(await controller.findAll()).toEqual([mockUserData]);
+    expect(service.findAll).toHaveBeenCalled();
+  });
+
+  it('should create a user', async () => {
+    expect(await controller.create(mockUserData)).toEqual(mockUserData);
+    expect(service.create).toHaveBeenCalledWith(mockUserData);
   });
 });
