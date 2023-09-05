f17b681f0d147dc490f76f40bb6b871f6ce78699
Dick Hon <honchunkin@gmail.com>
2023-09-06 06:51:17 +0800

    fix: express is not a function

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
