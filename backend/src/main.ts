6b03387c303ebffcebd0b88c1d2007bbc1219afa
Dick Hon <honchunkin@gmail.com>
2023-09-06 07:46:36 +0800

    feat: reset main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import serverlessExpress from '@vendia/serverless-express';
-import * as express from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.init();
-  return serverlessExpress({ app: server });
+  await app.listen(4000);
 }
-module.exports = bootstrap();
+bootstrap();
