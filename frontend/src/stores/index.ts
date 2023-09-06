e9d92f548eef776a9889fb9089b7be2f8ba45151
Dick Hon <honchunkin@gmail.com>
2023-09-06 11:04:04 +0800

    feat: add .env file

--- frontend/src/stores/index.ts
+++ frontend/src/stores/index.ts
@@ -1,7 +1,7 @@
 import AxiosAPI from "libs/api/axios";
 import {UserStore} from "./UserStore";
 
-const api = new AxiosAPI("http://localhost:4000");
+const api = new AxiosAPI(process.env.REACT_APP_API_URL || "");
 
 const userStore = new UserStore(api);
 
