2b2bb7297fdde9e21eddcea74804a468a29d9d3e
Dick Hon <honchunkin@gmail.com>
2023-09-06 12:11:51 +0800

    fix: add debug log

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
+  console.debug(
+    'process.env.FRONTEND_ORIGIN_URL: ',
+    process.env.FRONTEND_ORIGIN_URL,
+  );
+
   app.enableCors({
     origin: process.env.FRONTEND_ORIGIN_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
