fdacf9aeeb644939bfa3563436cade015a9bb89d
Dick Hon <honchunkin@gmail.com>
2023-09-06 11:47:56 +0800

    feat: add cors for frontend_origin_url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { config } from 'dotenv';
 import express from 'express';
 import { AppModule } from './app.module';
+
+config();
 
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_ORIGIN_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.init();
   return server;
